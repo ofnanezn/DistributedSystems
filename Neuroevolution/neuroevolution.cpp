3d6fc82db1f96be6eab466083def1e4b88705785
ofnanezn <ofnanezn@unal.edu.co>
2019-09-13 10:49:51 -0500

    Added weight initialization\

--- Neuroevolution/neuroevolution.cpp
+++ Neuroevolution/neuroevolution.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <math.h>
+#include <random>
 
 using namespace std;
 
@@ -16,14 +19,46 @@ class NeuralNetwork{
 		}
 
 		float* initialize_weights(int, int);
-		float* forward_propagate(float*, float*, float);
+		float* forward_propagate(float*, float*);
 		float cost(float*, float*)
-};
+};	
 
-float* NeuralNetwork::initialize_weights(int input_size, int output_size){
-	vector<vector<vector<float>>> W;
+vector<vector<float> NeuralNetwork::initialize_weights(int input_size, int output_size){
+	default_random_engine generator;
+  	normal_distribution<double> distribution(0.0,1.0);
 
+	vector<vector<float>> W;
+	
+	vector<float> W0(input_size * hidden_layers[0]);
+	for(int i = 0; i < W0.size(); ++i){
+		double r = distribution(generator);
+		double bound = sqrt(1.0 / (input_size + hidden_layers[0]));
+		W0[i] = r * bound;
+	}
+	W.push_back(W0);
+
+	for(int l = 1; l < num_layers; ++l){
+		vector<float> Wl(hidden_layers[l-1] * hidden_layers[l]);
+		for(int i = 0; i < Wl.size(); ++i){
+			double r = distribution(generator);
+			double bound = sqrt(1.0 / (hidden_layers[l] + hidden_layers[l-1]));
+			Wl[i] = r * bound;
+		}
+		W.push_back(Wl)
+	}
+
+	vector<float> WL(output_size * hidden_layers[num_layers-1]);
+	for(int i = 0; i < WL.size(); ++i){
+		double r = distribution(generator);
+		double bound = sqrt(1.0 / (output_size + hidden_layers[num_layers-1]));
+		WL[i] = r * bound;
+	}
+	W.push_back(WL);
+
+	return W;
 }
+
+vector<float> forward_propagate();
 
 int main(){
 	int num_layers = 4;
